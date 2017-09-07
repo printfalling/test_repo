99f407235c2216db723f977f25bd839ff7070a68
printfall <wjn2015@mail.ustc.edu.cn>
2017-09-07 20:45:43 +0800

    a test for pull request.

--- git_test/git_test.cpp
+++ git_test/git_test.cpp
@@ -2,10 +2,27 @@
 //
 
 #include "stdafx.h"
+#include <iostream>
+#include <cstdlib>
 
+using namespace std;
+
+int output()
+{
+	for (size_t i = 0; i < 20; i++)
+	{
+		cout << '*';
+	}
+	cout << endl;
+	return 0;
+}
 
 int main()
 {
+	cout << "Hello world!" << endl;
+	output();
     return 0;
 }
+
+
 
